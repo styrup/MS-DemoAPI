66370824ce34af8c423985e1a6ff67a5a3613e0c
styrup <37353298+styrup@users.noreply.github.com>
2019-08-16 08:48:06 +0200

    Fixed broken test :)

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Jacob";
+            return "Les Jackson";
         }
 
         // POST api/values
