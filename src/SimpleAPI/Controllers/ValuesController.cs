1de960e7b6108b45aec54e3c4e728fd38a0b23c8
styrup <37353298+styrup@users.noreply.github.com>
2019-08-16 09:36:29 +0200

    Changede get values

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "somedata", "rules" };
         }
 
         // GET api/values/5
